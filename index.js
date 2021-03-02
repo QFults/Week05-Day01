7846804d19817f8a42c21fbe8aeed5c86ee04b4f
Quinton Fults <quinton.fults.work@gmail.com>
2021-03-02 10:55:01 -0800

    code

---
+++ index.js
@@ -0,0 +1,16 @@
+// console.log('Hello World!')
+
+// let x = 5
+// x = x + 3
+// x = x / 2
+
+// console.log(x)
+
+// let dog = {
+//   name: 'Beef',
+//   age: 2
+// }
+
+// let arr = [1, 2, 3, 4]
+
+console.log(process.argv[4])
