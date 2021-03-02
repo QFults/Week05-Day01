f514c5fc04dba2e2dbcd178abdb46ded3bc5fdb1
Quinton Fults <quinton.fults.work@gmail.com>
2021-03-02 11:11:27 -0800

    code

--- index.js
+++ index.js
@@ -13,4 +13,12 @@
 
 // let arr = [1, 2, 3, 4]
 
-console.log(process.argv[4])
+// console.log(process.argv[4])
+
+let x = process.argv[2]
+
+if (!(x % 2)) {
+  console.log('The number is even')
+} else {
+  console.log('The number is odd')
+}
