d8cf7ce353ceb57e591910d318646b3cf0fc92e9
Quinton Fults <quinton.fults.work@gmail.com>
2021-03-02 13:31:14 -0800

    code

---
+++ bank/bank.js
@@ -0,0 +1,13 @@
+// Write code to make the following commands work
+
+// node bank.js balance
+// -- this command should sum together all amounts on bank.txt and display total
+
+// node bank.js deposit 50
+// -- this command should add the amount to the bank.txt
+
+// node bank.js withdraw 50
+// -- this command should add the negative amount to the bank.txt
+
+// node bank.js lotto
+// this command should withdraw 0.25 from the bank, run a 50/50 chance (random Math), and either withdraw or add 10 as a result
--- file.js
+++ file.js
@@ -29,4 +29,22 @@ const fs = require('fs')
 //   if (err) { console.log(err) }
 // })
 
+// let first = process.argv[2]
+// let last = process.argv[3]
 
+// let html = `<!DOCTYPE html>
+// <html lang="en">
+// <head>
+//   <meta charset="UTF-8">
+//   <meta http-equiv="X-UA-Compatible" content="IE=edge">
+//   <meta name="viewport" content="width=device-width, initial-scale=1.0">
+//   <title>Document</title>
+// </head>
+// <body>
+//   <h1>${first} ${last}</h1>
+// </body>
+// </html>`
+
+// fs.writeFile('index.html', html, err => {
+//   if (err) { console.log(err) }
+// })
