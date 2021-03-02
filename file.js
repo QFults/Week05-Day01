3985dc82639edf7fa5a6a4abff37f3df8859d2e7
Quinton Fults <quinton.fults.work@gmail.com>
2021-03-02 11:57:50 -0800

    code

---
+++ file.js
@@ -0,0 +1,26 @@
+const fs = require('fs')
+
+// fs.readFile('some.txt', 'utf8', (err, text) => {
+//   if (err) { console.log(err) }
+//   console.log(text)
+// })
+
+// fs.readFile('pet.json', 'utf8', (err, data) => {
+//   if (err) { console.log(err) }
+//   let pet = JSON.parse(data)
+//   console.log(pet.name)
+// })
+
+// fs.writeFile('some.txt', 'Something Different', err => {
+//   if (err) { console.log(err) }
+// })
+
+// let pet = {
+//   name: 'Bear',
+//   age: 6,
+//   breed: 'cockapoo'
+// }
+
+// fs.writeFile('pet.json', JSON.stringify(pet), err => {
+//   if (err) { console.log(err) }
+// })
